3cc9d0c26e2f679a05abcded950f0b9132daef1a
Edmund-Koo <e0696911@u.nus.edu>
2021-04-07 17:23:26 +0800

    Test2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -56,14 +56,14 @@ namespace ASPDotNetShoppingCart.Controllers
                  new Products()
                 {
                     productName = ".NET Analytics",
-                    price = 299,
+                    price = 399,
                     description = "Performs data mining and analytics easily in .NET.",
                     imagePath = "/img/NET_Analytics.png"
                 },
                 new Products()
                 {
                     productName = ".NET Logger",
-                    price = 49,
+                    price = 169,
                     description = "Logs and aggregates events easily in your .NET apps.",
                     imagePath = "/img/NET_Logger.png"
                 },
