6ae66611a9f530adea2eab2d0a3b80c855e45e70
shonsz <71485438+shonsz@users.noreply.github.com>
2021-04-15 11:01:06 +0800

    cart quantity

---
+++ wwwroot/js/Cart.js
@@ -0,0 +1,24 @@
+﻿window.onload = function () {
+    let decreaseQty = document.getElementsByClassName("cart-reduce");
+    for (let i = 0; i < decreaseQty.length; i++) {
+        decreaseQty[i].addEventListener("click", decreaseValue);
+    }
+    let increaseQty = document.getElementsByClassName("cart-add");
+    for (let i = 0; i < increaseQty.length; i++) {
+        increaseQty[i].addEventListener("click", increaseValue);
+    }
+}
+function increaseValue() {
+    let elem = event.currentTarget;
+    var value = parseInt(elem.getElementById('number').innerHTML);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').innerHTML = value;
+}
+
+function decreaseValue() {
+    var value = parseInt(document.getElementById('number').innerHTML);
+    value = isNaN(value) ? 0 : value;
+    value < 2 ? value = 1 : value--;
+    document.getElementById('number').innerHTML = value;
+}
