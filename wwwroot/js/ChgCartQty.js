cc3f13a20058543e40841ed24306a61de4d8b1de
Edmund-Koo <e0696911@u.nus.edu>
2021-04-15 13:50:57 +0800

    Commit

--- wwwroot/js/ChgCartQty.js
+++ wwwroot/js/ChgCartQty.js
@@ -7,18 +7,31 @@
     for (let i = 0; i < increaseQty.length; i++) {
         increaseQty[i].addEventListener("click", increaseValue);
     }
+    let qty = document.getElementsByClassName("quantity");
+    for (let i = 0; i < qty.length; i++) {
+        qty[i].addEventListener("click", updateSubtotal);
+    }
 }
-function increaseValue() {
+function increaseValue(event) {
     let elem = event.currentTarget;
-    var value = parseInt(document.getElementById('number').innerHTML);
+    let name = elem.getAttribute("data-product");
+    var value = parseInt(document.getElementById(name).innerHTML);
     value = isNaN(value) ? 0 : value;
     value++;
-    document.getElementById('number').innerHTML = value;
+    document.getElementById(name).innerHTML = value;
 }
 
-function decreaseValue() {
-    var value = parseInt(document.getElementById('number').innerHTML);
+function decreaseValue(event) {
+    let elem = event.currentTarget;
+    let name = elem.getAttribute("data-product");
+    var value = parseInt(document.getElementById(name).innerHTML);
     value = isNaN(value) ? 0 : value;
     value < 2 ? value = 1 : value--;
-    document.getElementById('number').innerHTML = value;
+    document.getElementById(name).innerHTML = value;
 }
+
+function updateSubtotal(event) {
+    
+    
+}
+
