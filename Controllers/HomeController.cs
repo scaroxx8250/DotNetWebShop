dab8e33132f642613864b44703eee4bd5c133f62
learner-9 <e0699394@u.nus.edu>
2021-04-16 18:20:56 +0800

    supress browser caching

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,6 +19,8 @@ namespace ASPDotNetShoppingCart.Controllers
             this.db = db;
         }
 
+        // suppress browser caching
+        [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
         public IActionResult Login()
         {
             string sessionId = Request.Cookies["sessionId"];
